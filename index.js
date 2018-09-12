46493da52fd4c95dc87e766e9d07944526fd5ee5
HUANG <yipeng.huang@cgi.com>
2018-09-12 17:11:48 +0200

    fix aframe 0.8.x

--- index.js
+++ index.js
@@ -23,10 +23,7 @@ AFRAME.registerComponent('crawling-cursor', {
     multiple: false,
 
     init: function() {
-        var el = this.el;
-        var data = this.data;
-
-        if (data.target === null) {
+        if (this.data.target === null) {
             var cursor = document.querySelector("a-cursor");
 
             if (cursor === null) {
@@ -34,43 +31,37 @@ AFRAME.registerComponent('crawling-cursor', {
                 return;
             }
 
-            data.target = cursor;
+            this.data.target = cursor;
         }
+    },
 
-        el.addEventListener("raycaster-intersection", function(e) {
+    tick: function () {
+        var intersection = getNearestIntersection(this.el.components.raycaster.intersections, this.data);
+        if (!intersection) { return; }
 
-            var intersection = getNearestIntersection(e.detail.intersections);
-            if (!intersection) { return; }
+        // a matrix which represents item's movement, rotation and scale on global world
+        var mat = intersection.object.matrixWorld;
+        // remove parallel movement from the matrix
+        mat.setPosition(new THREE.Vector3(0, 0, 0));
 
-            // a matrix which represents item's movement, rotation and scale on global world
-            var mat = intersection.object.matrixWorld;
-            // remove parallel movement from the matrix
-            mat.setPosition(new THREE.Vector3(0, 0, 0));
+        // change local normal into global normal
+        var global_normal = intersection.face.normal.clone().applyMatrix4(mat).normalize();
 
-            // change local normal into global normal
-            var global_normal = intersection.face.normal.clone().applyMatrix4(mat).normalize();
+        // look at target coordinate = intersection coordinate + global normal vector
+        var lookAtTarget = new THREE.Vector3().addVectors(intersection.point, global_normal);
+        this.data.target.object3D.lookAt(lookAtTarget);
 
-            // look at target coordinate = intersection coordinate + global normal vector
-            var lookAtTarget = new THREE.Vector3().addVectors(intersection.point, global_normal);
-            data.target.object3D.lookAt(lookAtTarget);
+        // cursor coordinate = intersection coordinate + normal vector * offset
+        var cursorPosition = new THREE.Vector3().addVectors(intersection.point, global_normal.multiplyScalar(this.data.offset));
+        this.data.target.setAttribute("position", cursorPosition);
 
-            // cursor coordinate = intersection coordinate + normal vector * offset
-            var cursorPosition = new THREE.Vector3().addVectors(intersection.point, global_normal.multiplyScalar(data.offset));
-            data.target.setAttribute("position", cursorPosition);
-
-            function getNearestIntersection(intersections) {
-                for (var i = 0, l = intersections.length; i < l; i++) {
-
-                    // ignore cursor itself to avoid flicker && ignore "ignore-ray" class
-                    if (data.target === intersections[i].object.el || intersections[i].object.el.classList.contains("ignore-ray")) { continue; }
-                    return intersections[i];
-                }
-                return null;
+        function getNearestIntersection(intersections, data) {
+            for (var i = 0, l = intersections.length; i < l; i++) {
+                // ignore cursor itself to avoid flicker && ignore "ignore-ray" class
+                if (data.target === intersections[i].object.el || intersections[i].object.el.classList.contains("ignore-ray")) { continue; }
+                return intersections[i];
             }
-        });
-
-        setInterval(function() {
-            el.components.raycaster.refreshObjects();
-        }, 100)
+            return null;
+        }
     }
 });
